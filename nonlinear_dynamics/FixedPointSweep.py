949e8baeee17ab633b16e5f42e680a4130983eac
doddgray <dodd@stanford.edu>
2019-12-28 20:00:08 -0500

    fixed typo

--- nonlinear_dynamics/FixedPointSweep.py
+++ nonlinear_dynamics/FixedPointSweep.py
@@ -384,7 +384,7 @@ def jac_eigvals_sweep(Pa,Pb,Δ,s=np.sqrt(20.),γ=2.,μ=30.,r=0.2,ζ=0.1,τ_th=30
                 # print(f'inds: {inds}')
                 # print(f'a[inds]: {a[inds]}')
             j = jacobian(a[inds],b[inds],na[inds],nb[inds],T[inds],Δ=Δ[inds[0]],s=s,
-                γ=γ,μ=μ,r=r,ζ=ζ,τ_th=τ_th,η1=η1,η2=η2,τ_fc=τ_fc,τ_xfc=τ_xfc,χ=χ,α=α,,η=η)
+                γ=γ,μ=μ,r=r,ζ=ζ,τ_th=τ_th,η1=η1,η2=η2,τ_fc=τ_fc,τ_xfc=τ_xfc,χ=χ,α=α,η=η)
             try:
                 eigvals[inds,:] = np.linalg.eigvals(j)
                 det_j[inds] = np.linalg.det(j)
